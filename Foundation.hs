9fa38a8d42dff0f5bb53f097ba3dbc55fc4e2c73
Austin Erlandson <austin@erlandson.com>
2017-04-22 18:33:26 -0500

    Finished the tut.

--- Foundation.hs
+++ Foundation.hs
@@ -137,6 +137,8 @@ instance Yesod App where
     isAuthorized (AuthR _) _ = return Authorized
     isAuthorized CommentR _ = return Authorized
     isAuthorized HomeR _ = return Authorized
+    isAuthorized PostNewR _ = return Authorized
+    isAuthorized (PostDetailsR blogPostId) _ = return Authorized
     isAuthorized FaviconR _ = return Authorized
     isAuthorized RobotsR _ = return Authorized
     isAuthorized (StaticR _) _ = return Authorized
